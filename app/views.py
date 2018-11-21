c65b8e25de64ec4845d0b0420c0a319f4ad46146
Oshan Wisumperuma <warunaoshan@gmail.com>
2018-11-21 14:40:49 +0530

    test get request

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def webhooks(request):
+    response_data = {
+        'id': 4,
+        'name': 'Test Response',
+        'roles': ['Admin', 'User']
+    }
+
+    return JsonResponse(response_data)
